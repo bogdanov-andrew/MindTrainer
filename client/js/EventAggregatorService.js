dba8ce58f0dfd11cbdb83e65af5c152a1d851e79
Bogdanov Andrew <bogdanov.andrew.i@gmail.com>
2016-07-15 18:22:41 +0300

    event aggregator

---
+++ client/js/EventAggregatorService.js
@@ -0,0 +1,41 @@
+﻿var module = angular.module('eventAggregatorModule', []);
+function EventAggregatorService() {
+
+    var subscriptionList = [];
+
+    this.isSubscriptionExists = function(eventName, action) {
+
+    };
+
+    this.Subscribe = function(eventName, action) {
+        if (!isSubscriptionExists(eventName, action)) {
+
+            var subscription = {
+                eventName: eventName,
+                action: action
+            };
+
+            subscriptionList.push(subscription);
+        }
+    };
+    
+    this.Publish = function (eventName) {
+        var arr = subscriptionList.filter(function(element) {
+            return element.eventName == eventName;
+        });
+
+        arr.forEach(function (item) {
+            item.action();
+        });
+    };
+
+    this.UnSubscribe = function(eventName, action) {
+
+    };
+
+    return {
+        subscribe: this.Subscribe,
+        publish: this.Publish
+    };
+};
+module.factory('eventAggregatorService', EventAggregatorService);
